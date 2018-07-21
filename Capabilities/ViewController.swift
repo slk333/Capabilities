42f41228bf4be4b59633f3fa8c3382e467666714
Antoine Weber <aweb33@icloud.com>
2018-07-21 11:13:54 +0200

    initial commit

--- Capabilities/ViewController.swift
+++ Capabilities/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var gestureLabel: UILabel!
+    
+    @IBAction func swipe(_ sender: UISwipeGestureRecognizer) {
+        gestureLabel.text = "swiped"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
