c49b0eff3255af7df62ca9a0b2a98905da92bb79
Antoine Weber <aweb33@icloud.com>
2018-07-21 09:24:46 +0200

    Initial Commit

---
+++ Capabilities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Capabilities
+//
+//  Created by Antoine Weber on 21/07/2018.
+//  Copyright © 2018 Antoine Weber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
