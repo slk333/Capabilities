a1297e8a3cc7a04740b10c792f768cf55479ab96
slk333 <aweb33@icloud.com>
2019-04-30 22:59:55 +0200

    clean up code and use better colors

--- Capabilities/AnimationViewController.swift
+++ Capabilities/AnimationViewController.swift
@@ -1,8 +1,38 @@
+   
+   /*
+    let transform = CATransform3DMakeTranslation(
+    100,
+    0,
+    0
+    )
+    let animation = CABasicAnimation(keyPath: "transform")
+    animation.toValue = NSValue(caTransform3D: transform)
+    animation.duration = 3
+    face1.layer.add(animation, forKey: "transform")
+    */
+   
+   /*
+    
+    
+    
+    let scaleFactor = CGFloat.random(in: 0...2)
+    
+    let scaleTransform = CATransform3DMakeScale(
+    scaleFactor,
+    scaleFactor,
+    1
+    )
+    
+    */
+   
+
+
+
 import UIKit
-  var coeff:CGFloat = 2.0
+var coeff:CGFloat = 2.0
 class AnimationViewController: UIViewController {
     
-  
+    
     
     @IBOutlet weak var image: UIImageView!
     
@@ -62,79 +92,50 @@ class AnimationViewController: UIViewController {
     
     func rotateCube(){
         let rotationTransform = CATransform3DMakeRotation(
-            .random(in: 0...(.pi)
-            ),
+            .random(in: 0...(.pi)),
             .random(in: -1...1),
             .random(in: -1...1),
             0.0
         )
-        
-        
-        
-        
-        /*
-         
-         let translateTransform = CATransform3DMakeTranslation(
-         .random(in: -50...50),
-         .random(in: -50...50),
-         .random(in: -50...50)
-         )
-         
-         let scaleFactor = CGFloat.random(in: 0...2)
-         
-         let scaleTransform = CATransform3DMakeScale(
-         scaleFactor,
-         scaleFactor,
-         1)
-         
-         let resultTransform = CATransform3DConcat(
-         translateTransform,
-         CATransform3DConcat(
-         rotationTransform,
-         scaleTransform)
-         )
-         
-         */
-        
-        
+       
         
         
         let resultTransformLeft = CATransform3DConcat(
-            self.rotationTransformYAxis,
+            rotationTransformYAxis,
             CATransform3DConcat(
-                self.translateTransformLeft ,
+                translateTransformLeft ,
                 rotationTransform
             )
         )
         let resultTransformRight = CATransform3DConcat(
-            self.rotationTransformYAxis ,
+            rotationTransformYAxis ,
             CATransform3DConcat(
-                self.translateTransformRight,
+                translateTransformRight,
                 rotationTransform
             )
         )
         let resultTransformTop = CATransform3DConcat(
-            self.rotationTransformXAxis ,
+            rotationTransformXAxis ,
             CATransform3DConcat(
-                self.translateTransformTop,
+                translateTransformTop,
                 rotationTransform
             )
         )
         let resultTransformDown = CATransform3DConcat(
-            self.rotationTransformXAxis ,
+            rotationTransformXAxis ,
             CATransform3DConcat(
-                self.translateTransformDown,
+                translateTransformDown,
                 rotationTransform
             )
         )
         
         let resultTransformBehind = CATransform3DConcat(
-            self.translateTransformBehind,
+            translateTransformBehind,
             rotationTransform
         )
         
         let resultTransformFront = CATransform3DConcat(
-            self.translateTransformFront,
+            translateTransformFront,
             rotationTransform
         )
         
@@ -143,147 +144,36 @@ class AnimationViewController: UIViewController {
         
         //  self.image.layer.transform = resultTransform
         
-        self.face1.layer.transform = resultTransformFront
-        self.face2.layer.transform = resultTransformBehind
-        self.face3.layer.transform = resultTransformLeft
-        self.face4.layer.transform = resultTransformRight
-        self.face5.layer.transform = resultTransformTop
-        self.face6.layer.transform = resultTransformDown
+        face1.layer.transform = resultTransformFront
+        face2.layer.transform = resultTransformBehind
+        face3.layer.transform = resultTransformLeft
+        face4.layer.transform = resultTransformRight
+        face5.layer.transform = resultTransformTop
+        face6.layer.transform = resultTransformDown
     }
     
-    @IBAction func reset(_ sender: Any) {
+    @IBAction func instant(_ sender: Any) {
         
-    rotateCube()
+        rotateCube()
     }
     
     
     @IBAction func animate(_ sender: UIBarButtonItem) {
         UIView.animate(withDuration: 1.0, delay: 0.0, options: [.curveEaseInOut], animations: {
-       
-            //    self.image.layer.position.x += CGFloat(Float.random(in: -50...50))
-            //     self.image.layer.position.y += CGFloat(Float.random(in: -50...50))
-            
-            //  self.image.frame = self.image.frame.offsetBy(dx: .random(in: -50...50), dy: .random(in: -50...50))
-            
-            
-            
-            
-            
-            let rotationTransform = CATransform3DMakeRotation(
-                .random(in: 0...0.7
-                ),
-                .random(in: -1...1),
-                .random(in: -1...1),
-                0.0
-            )
-            
-            
-            
-            
-            /*
-             
-             let translateTransform = CATransform3DMakeTranslation(
-             .random(in: -50...50),
-             .random(in: -50...50),
-             .random(in: -50...50)
-             )
-             
-             let scaleFactor = CGFloat.random(in: 0...2)
-             
-             let scaleTransform = CATransform3DMakeScale(
-             scaleFactor,
-             scaleFactor,
-             1)
-             
-             let resultTransform = CATransform3DConcat(
-             translateTransform,
-             CATransform3DConcat(
-             rotationTransform,
-             scaleTransform)
-             )
-             
-             */
-            
-          
-            
-            
-            let resultTransformLeft = CATransform3DConcat(
-                self.rotationTransformYAxis,
-                CATransform3DConcat(
-                    self.translateTransformLeft ,
-                    rotationTransform
-                )
-            )
-            let resultTransformRight = CATransform3DConcat(
-                self.rotationTransformYAxis ,
-                CATransform3DConcat(
-                    self.translateTransformRight,
-                    rotationTransform
-                )
-            )
-            let resultTransformTop = CATransform3DConcat(
-                self.rotationTransformXAxis ,
-                CATransform3DConcat(
-                    self.translateTransformTop,
-                    rotationTransform
-                )
-            )
-            let resultTransformDown = CATransform3DConcat(
-                self.rotationTransformXAxis ,
-                CATransform3DConcat(
-                    self.translateTransformDown,
-                    rotationTransform
-                )
-            )
-            
-            let resultTransformBehind = CATransform3DConcat(
-                self.translateTransformBehind,
-                rotationTransform
-            )
-            
-            let resultTransformFront = CATransform3DConcat(
-                self.translateTransformFront,
-                rotationTransform
-            )
-            
-            
-            
-            
-            //  self.image.layer.transform = resultTransform
-            
-            self.face1.layer.transform = resultTransformFront
-            self.face2.layer.transform = resultTransformBehind
-            self.face3.layer.transform = resultTransformLeft
-            self.face4.layer.transform = resultTransformRight
-            self.face5.layer.transform = resultTransformTop
-            self.face6.layer.transform = resultTransformDown
+            self.rotateCube()
             
         }, completion: nil)
-            
-      
-            
         
+ 
     }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        image.isHidden = true
-        
-        
+    func createFaces(){
         let faceSize = CGSize(width: 100 * coeff, height: 100 * coeff)
-        let center = CGPoint(x:view.frame.midX,y:view.frame.midY)
+        let center = view.center
+        
+        
+        
         
         face1 = UIView(frame: CGRect(
             origin: center,
@@ -314,50 +204,61 @@ class AnimationViewController: UIViewController {
         let pink = UIColor(hue: 0.9056, saturation: 1, brightness: 0.89, alpha: 1.0).cgColor
         let lightPink = UIColor(hue: 0.9222, saturation: 0.3, brightness: 0.92, alpha: 1.0).cgColor
         let lightPurple = UIColor(hue: 0.8, saturation: 0.47, brightness: 0.56, alpha: 1.0).cgColor
-
-
         
         
+        
+        let verticalOffset = navigationController!.navigationBar.frame.size.height + UIApplication.shared.statusBarFrame.height
         
         
         face1.layer.backgroundColor = pink
-
         face2.layer.backgroundColor = pink
-        
         face3.layer.backgroundColor = lightPink
-
         face4.layer.backgroundColor = lightPink
         face5.layer.backgroundColor = lightPurple
         face6.layer.backgroundColor = lightPurple
         
-        face2.layer.position = center
-        face1.layer.position = center
-        face3.layer.position = center
-        face4.layer.position = center
-        face5.layer.position = center
-        face6.layer.position = center
+        let correctCenter = CGPoint(x: center.x, y: center.y - verticalOffset)
+        
+        face1.center = correctCenter
+        face2.center = correctCenter
+        face3.center = correctCenter
+        face4.center = correctCenter
+        face5.center = correctCenter
+        face6.center = correctCenter
+        
+        
+        print(verticalOffset)
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        view.addSubview(face1)
         view.addSubview(face2)
         view.addSubview(face3)
         view.addSubview(face4)
         view.addSubview(face5)
         view.addSubview(face6)
+        view.addSubview(face1)
         
+    }
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        image.isHidden = true
         
+    
+        
+        createFaces()
         rotateCube()
         
+    print(view.center.x,view.center.y,view.bounds.minX,view.bounds.maxX,view.bounds.minY,view.bounds.maxY)
         
-        
+        print(view.frame)
+        print(view.bounds)
         
     }
 }
